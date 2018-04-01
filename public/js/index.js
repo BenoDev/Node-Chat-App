aedf1ce9bf6d49548aa019615ebca90c1143826e
BenoDev <ciro.vassallo90@gmail.com>
2018-04-01 19:56:56 +0200

    Add form for messages

--- public/js/index.js
+++ public/js/index.js
@@ -11,4 +11,20 @@ var socket = io(); //make request to server to open connection
 
     	socket.on('newMessage', function(message){
     		console.log('New Message',message);
+    		let li = jQuery('<li></li>');
+    		li.text(`${message.from}: ${message.text}`);
+
+    		jQuery('#messages').append(li);
     	});
+
+    
+
+    	jQuery('#message-form').on('submit',function(e){
+			e.preventDefault();
+			socket.emit('createMessage',{
+				from: 'User',
+				text: jQuery('[name=message').val()
+			},function(){
+
+			});
+    	})
