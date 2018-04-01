fb0ea493870cf66c0f896cf4ea0a33c3bff1f481
BenoDev <ciro.vassallo90@gmail.com>
2018-04-01 02:18:47 +0200

    add socket.io

--- server/server.js
+++ server/server.js
@@ -1,14 +1,29 @@
 const path = require('path'); //standard node module
+const http = require('http'); //standard node module
 const express = require('express');
+const socketIO = require('socket.io')
 
 const publicPath = path.join(__dirname,'..','/public');
-let PORT = process.env.PORT || 3000;
+let port = process.env.PORT || 3000;
 
-const app = express();
+let app = express();
+let server = http.Server(app);
+let io = socketIO(server);
+
 
 app.use(express.static(publicPath));
 
+//add and event lisnet connection
+io.on('connection', (socket)=> {
+	console.log('New user connected');
+	socket.on('disconnect',()=>{
+		console.log('User was disconnected')
+	})
+});
 
-app.listen(PORT, ()=> {
-  console.log(`Server is up on port ${PORT}!`);
+	
+
+
+server.listen(port, ()=> {
+  console.log(`Server is up on port ${port}!`);
 });
