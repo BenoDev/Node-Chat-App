73c00d8f313b959f637461dc6d89cfb2bbe26184
BenoDev <ciro.vassallo90@gmail.com>
2018-04-02 23:23:57 +0200

    Formate html with Mustache.js templating

--- public/js/index.js
+++ public/js/index.js
@@ -10,23 +10,38 @@ var socket = io(); //make request to server to open connection
     	});
 
     	socket.on('newMessage', function(message){
-    		let formattedTime = moment(message.createdAt).format('h:mm a');
+    		var formattedTime = moment(message.createdAt).format('h:mm a');
+    		var template = jQuery('#message-template').html();
+    		var html = Mustache.render(template,{
+    			text: message.text,
+    			from: message.from,
+    			createdAt:formattedTime
+    		});
+    		jQuery('#messages').append(html)
 
-    		let li = jQuery('<li></li>');
-    		li.text(`${message.from} ${formattedTime}: ${message.text}`);
+    		// let li = jQuery('<li></li>');
+    		// li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
-    		jQuery('#messages').append(li);
+    		// jQuery('#messages').append(li);
     	});
 
     	socket.on('newLocationMessage', function(message){
-    		let formattedTime = moment(message.createdAt).format('h:mm a');
+    		var formattedTime = moment(message.createdAt).format('h:mm a');
+    		var template = jQuery('#location-message-template').html();
+    		var html = Mustache.render(template,{
+    			text: message.text,
+    			url: message.url,
+    			createdAt:formattedTime
+    		});
+    		jQuery('#messages').append(html)
+   //  		var formattedTime = moment(message.createdAt).format('h:mm a');
 
-			var li = jQuery('<li></li>');
-			var a = jQuery('<a target = "_blank">My current location</a>');
-			li.text(`${message.from} ${formattedTime}: `);
-			a.attr('href',message.url);
-			li.append(a);
-			jQuery('#messages').append(li);
+			// var li = jQuery('<li></li>');
+			// var a = jQuery('<a target = "_blank">My current location</a>');
+			// li.text(`${message.from} ${formattedTime}: `);
+			// a.attr('href',message.url);
+			// li.append(a);
+			// jQuery('#messages').append(li);
     	});
 
     	jQuery('#message-form').on('submit',function(e){
