aa34f6918f35d4960d93e9f6e8ebeebed7294d3f
BenoDev <ciro.vassallo90@gmail.com>
2018-04-03 03:33:54 +0200

    Add Users class and updateUserList event

--- public/js/chat.js
+++ public/js/chat.js
@@ -32,6 +32,15 @@ var socket = io(); //make request to server to open connection
     		console.log('Disconnected from the server')
     	});
 
+    	socket.on('updateUserList', function(users){
+			var ol = jQuery('<ol></ol>')
+
+			users.forEach(function(user){
+				ol.append(jQuery('<li></li>').text(user))
+			})
+			jQuery('#users').html(ol);
+    	})
+
     	socket.on('newMessage', function(message){
     		var formattedTime = moment(message.createdAt).format('h:mm a');
     		var template = jQuery('#message-template').html();
--- server/server.js
+++ server/server.js
@@ -1,16 +1,18 @@
 const path = require('path'); //standard node module
 const http = require('http'); //standard node module
 const express = require('express');
-const socketIO = require('socket.io')
+const socketIO = require('socket.io');
 
 const {generateMessage,generateLocationMessage} = require('./utils/message');
-const {isRealString} = require('./utils/validations')
+const {isRealString} = require('./utils/validations');
+const {Users} = require('./utils/users')
 const publicPath = path.join(__dirname,'..','/public');
 let port = process.env.PORT || 3000;
 
 let app = express();
 let server = http.Server(app);
 let io = socketIO(server);
+let users = new Users();
 
 
 app.use(express.static(publicPath));
@@ -24,9 +26,13 @@ io.on('connection', (socket)=> {
 
 	socket.on('join',(params,callback)=>{
 		if(!isRealString(params.name) || !isRealString(params.room)){
-			callback('Name and room name are required')
+			return callback('Name and room name are required')
 		}
 		socket.join(params.room);
+		users.removeUser(socket.id);
+		users.addUser(socket.id,params.name,params.room);
+
+		io.to(params.room).emit('updateUserList',users.getUserList(params.room))
 
 
 		socket.emit('newMessage',generateMessage('Admin','Welcome to the chat app'));
@@ -46,6 +52,11 @@ io.on('connection', (socket)=> {
 		});
 
 	socket.on('disconnect',()=>{
+		let user = users.removeUser(socket.id);
+		if(user){
+			io.to(user.room).emit('updateUserList',users.getUserList(user.room))
+			io.to(user.room).emit('newMessage',generateMessage('Admin', `${user.name} has left`))
+		}
 		console.log('User was disconnected')
 	});
 });
